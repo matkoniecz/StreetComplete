9aa09589524058f8e3bf01ce67247601eda037d1
Mateusz Konieczny <matkoniecz@gmail.com>
2021-04-15 19:52:19 +0200

    barrier quest select

--- app/src/main/java/de/westnordost/streetcomplete/quests/barrier_type/AddBarrierTypeForm.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/barrier_type/AddBarrierTypeForm.kt
@@ -8,11 +8,28 @@ import de.westnordost.streetcomplete.view.image_select.Item
 class AddBarrierTypeForm : AImageListQuestAnswerFragment<BarrierType, BarrierType>() {
 
     override val items = listOf(
-        Item(GATE, R.drawable.bicycle_parking_type_stand, R.string.quest_bicycle_parking_type_stand),
-        Item(LOG, R.drawable.bicycle_parking_type_stand, R.string.quest_bicycle_parking_type_stand),
+        Item(GATE, R.drawable.barrier_gate, R.string.quest_barrier_type_gate), // optimize description?
+        Item(LIFT_GATE, R.drawable.barrier_lift_gate, R.string.quest_barrier_type_lift_gate), // AKA boom barrier TODO which term use?
+        Item(BOLLARD, R.drawable.barrier_bollard, R.string.quest_barrier_type_bollard),
+        Item(CHAIN, R.drawable.barrier_chain, R.string.quest_barrier_type_chain),
+        Item(ROPE, R.drawable.barrier_rope, R.string.quest_barrier_type_rope),
+        Item(WIRE_GATE, R.drawable.barrier_wire_gate, R.string.quest_barrier_type_wire_gate),
+        Item(CATTLE_GRID, R.drawable.barrier_cattle_grid, R.string.quest_barrier_type_cattle_grid),
+        Item(BLOCK, R.drawable.barrier_block, R.string.quest_barrier_type_block),
+        Item(JERSEY_BARRIER, R.drawable.barrier_jersey_barrier, R.string.quest_barrier_jersey_barrier),
+        Item(LOG, R.drawable.barrier_log, R.string.quest_barrier_type_log),
+        Item(KERB, R.drawable.kerb_height_raised, R.string.quest_barrier_type_kerb), // TODO: translate it as curb in UK!
+        Item(HEIGHT_RESTRICTOR, R.drawable.barrier_height_restrictor, R.string.quest_barrier_type_height_restrictor),
+        Item(FULL_HEIGHT_TURNSTILE, R.drawable.barrier_full_height_turnstile, R.string.quest_barrier_full_height_turnstile),
+        Item(TURNSTILE, R.drawable.barrier_turnstile, R.string.quest_barrier_type_turnstile),
+        Item(PASSAGE, R.drawable.barrier_passage, R.string.quest_barrier_type_passage),
+        Item(DEBRIS_PILE, R.drawable.barrier_debris_pile, R.string.quest_barrier_type_debris_pile),
+        Item(STILE_SQUEEZER, R.drawable.barrier_stile_squeezer, R.string.quest_barrier_type_stile_squeezer),
+        Item(STILE_LADDER, R.drawable.barrier_stile_ladder, R.string.quest_barrier_type_stile_ladder),
+
     )
 
-    override val itemsPerRow = 3
+    override val itemsPerRow = 4
 
     override fun onClickOk(selectedItems: List<BarrierType>) {
         applyAnswer(selectedItems.single())
--- app/src/main/java/de/westnordost/streetcomplete/quests/barrier_type/BarrierType.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/barrier_type/BarrierType.kt
@@ -1,6 +1,35 @@
 package de.westnordost.streetcomplete.quests.bike_parking_type
 
 enum class BarrierType(val osmValue: String) {
-    GATE("gate"),
-    LOG("log"),
+    // /home/mateusz/Documents/install_moje/rescale-for-android
+    GATE("gate"), // also fitting for swing gate: https://wiki.openstreetmap.org/wiki/File:Lovely_gates_near_Hollinshead_Swing_Bridge_-_geograph.org.uk_-_844479.jpg
+    LIFT_GATE("lift_gate"), // https://wiki.openstreetmap.org/wiki/File:Lift_gate.jpg
+    BOLLARD("bollard"), // https://commons.wikimedia.org/wiki/File:Stilpoller_Kugelkopf.jpg
+    CHAIN("chain"), // https://commons.wikimedia.org/wiki/File:Les_Ulis_le_23_ao%C3%BBt_2012_-_21.jpg
+    ROPE("rope"), // https://wiki.openstreetmap.org/wiki/File:Black_Red_Gold_Rope_at_German_Bundestag_in_Berlin_2010.jpg
+    WIRE_GATE("hampshire_gate"),
+    CATTLE_GRID("cattle_grid"),// https://wiki.openstreetmap.org/wiki/File:Cattle_grid.jpg
+    BLOCK("block"), // https://commons.wikimedia.org/wiki/File:Oblast_mezi_Libe%C5%88sk%C3%BDm_mostem_a_Negrelliho_viaduktem_(06).jpg
+    JERSEY_BARRIER("jersey_barrier"), // https://commons.wikimedia.org/wiki/File:BarreiraNewJersey.JPG https://commons.wikimedia.org/wiki/Category:Jersey_barriers
+    LOG("log"), // https://commons.wikimedia.org/wiki/Category:Logs_across_paths https://commons.wikimedia.org/wiki/File:Sihlwald_windthrow_Spinnerweg_20200204_1.jpg
+    KERB("curb"), // ????????????????????????
+    HEIGHT_RESTRICTOR("height_restrictor"), // https://commons.wikimedia.org/wiki/File:Sainsbury%27s_car_park_height_restriction_barrier,_Chingford,_London,_England_1.jpg
+    FULL_HEIGHT_TURNSTILE("full-height_turnstile"), // https://en.wikipedia.org/wiki/File:Type_B_Series_Roto-Gate.jpg
+    TURNSTILE("turnstile"), // https://commons.wikimedia.org/wiki/File:%D0%A2%D1%83%D1%80%D0%BD%D0%B8%D0%BA%D0%B5%D1%82%D1%8B_%D0%B4%D0%BB%D1%8F_%D0%BC%D0%BE%D0%B4%D1%83%D0%BB%D1%8F_%22%D0%92%D1%85%D0%BE%D0%B4-%D0%B2%D1%8B%D1%85%D0%BE%D0%B4%22.JPG
+    PASSAGE("entrance"), // https://wiki.openstreetmap.org/wiki/File:Archway_between_walled_gardens,_Greenway_-_geograph.org.uk_-_191221.jpg # https://commons.wikimedia.org/wiki/File:Doorgang_in_muur._Locatie,_Chinese_tuin_Het_Verborgen_Rijk_van_Ming._Locatie._Hortus_Haren_01.jpg?fastcci_from=69893&c1=69893&d1=15&s=200&a=fqv
+    DEBRIS_PILE("debris"), //??????????? // https://wiki.openstreetmap.org/wiki/File:Barrier_debris.jpg
+    STILE_SQUEEZER("stile"),// https://en.wikipedia.org/wiki/File:Squeezer_stile._-_geograph.org.uk_-_110502.jpg
+    STILE_LADDER("stile"), // https://en.wikipedia.org/wiki/File:Ladder_stile_Snowdonia.jpg
+    STILE_STEPOVER("stile"),
+    KISSING_GATE("kissing_gate"),  //https://commons.wikimedia.org/wiki/File:Kissing_gate_near_Trillinghurst_Farm_-_geograph.org.uk_-_1512534.jpg https://commons.wikimedia.org/wiki/File:Kissing_Gate_above_Colvithick_Wood_-_geograph.org.uk_-_109914.jpg
 }
+
+/*
+(bus trap)
+cycle barrier
+(horse stile)
+(motorcycle barrier)
+spikes
+stile
+sump buster
+*/
